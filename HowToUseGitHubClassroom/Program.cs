95ac962eedb03bf821dc8ada2d253ddd717afde9
Al Stone <astone@heckgrammar.co.uk>
2024-06-28 08:59:38 +0100

    Add project files.

---
+++ HowToUseGitHubClassroom/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HowToUseGitHubClassroom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter your code where there is a prompt or to complete a task
+            // Commit your changes and push to github
+            // Use the test data if given
+            // Screenshot your program running with the test data given (if available)
+            // If no test data is given, screenshot with your own test data
+            // Open GitHub.com and go to your program repository
+            // Add a README
+            // Paste the evidence of your program running
+            // repeat for other tests if needed
+            // Commit changes
+
+            // Practice task
+            // Write a program that asks you to enter your name
+            // Store the user input in an appropriate variable
+            // Output to the console the variable
+            // Output to the console Hello World
+            // Test your program and add the evidence to this repository
+        }
+    }
+}
